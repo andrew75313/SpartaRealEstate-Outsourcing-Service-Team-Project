815bed067f64b258052fec7102c4f3972e8415bb
Hyeonjin Kim <andrew75313@gmail.com>
2024-06-19 17:08:10 +0900

    git init

---
+++ src/main/java/com/sparta/realestatefeed/RealEstateFeedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.realestatefeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/realestatefeed/RealEstateFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.realestatefeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
