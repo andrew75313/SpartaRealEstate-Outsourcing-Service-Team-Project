675822414a9294ee5127d46c98846514b3dfefb3
Hyeonjin Kim <andrew75313@gmail.com>
2024-06-20 11:22:39 +0900

    feat: Spring Security 인증 처리를 위한 UserDetails, UserDetailsServiceImpl 생성
    -AuthenticationManager 인증 처리를  위한 UserRepository에서 User 찾기 쿼리 메서드 추가

--- src/main/java/com/sparta/realestatefeed/repository/UserRepository.java
+++ src/main/java/com/sparta/realestatefeed/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.sparta.realestatefeed.repository;
 import com.sparta.realestatefeed.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByUsername(String username);
 }
