e43407526848bdf5095bc66e6a7000e81d59e69c
HongYongGuen <163097270+HongYongGuen@users.noreply.github.com>
2024-06-20 10:23:26 +0900

    test :
    - 공통 responseDto 확인
    
    -pacagke 이름 설정, global exception handler
    -ignore에 secert Properti 무시
    -기초 클래스 추가

--- src/main/java/com/sparta/realestatefeed/controller/UserController.java
+++ src/main/java/com/sparta/realestatefeed/controller/UserController.java
@@ -5,6 +5,7 @@ import com.sparta.realestatefeed.dto.Response;
 import com.sparta.realestatefeed.util.ApiUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
