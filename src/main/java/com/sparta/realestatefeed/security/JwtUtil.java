0a427c643ab156be3d8e37831ade53f569d0fcfb
Hyeonjin Kim <andrew75313@gmail.com>
2024-06-20 12:03:38 +0900

    feat: 필터에서 JWT 인증 인가에 필요한 Util 클래스 생성
    -Access Token, Refresh Token 생성 메서드 추가
    -요청 Header에서 Access Token, Refresh Token get 메서드 추가
    -토큰 유효성 검사 메서드 추가
    -토큰에서 user 정보 get 메서드 추가

---
+++ src/main/java/com/sparta/realestatefeed/security/JwtUtil.java
@@ -0,0 +1,85 @@
+package com.sparta.realestatefeed.security;
+
+import com.sparta.realestatefeed.config.JwtConfig;
+import com.sparta.realestatefeed.entity.UserRoleEnum;
+import com.sparta.realestatefeed.repository.UserRepository;
+import io.jsonwebtoken.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+
+@Slf4j(topic = "JwtUtil")
+@Component
+public class JwtUtil {
+
+    private final UserRepository userRepository;
+
+    public JwtUtil(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String createAccessToken(String username, UserRoleEnum role) {
+
+        Date date = new Date();
+
+        return Jwts.builder()
+                .setSubject(username)
+                .claim(JwtConfig.AUTHORIZATION_KEY, role)
+                .setExpiration(new Date(date.getTime() + JwtConfig.ACCESS_TOKEN_TIME))
+                .setIssuedAt(date)
+                .signWith(JwtConfig.key, JwtConfig.SIGNATURE_ALGORITHM)
+                .compact();
+    }
+
+    public String createRefreshToken(String username) {
+
+        Date date = new Date();
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(date.getTime() + JwtConfig.REFRESH_TOKEN_TIME))
+                .setIssuedAt(date)
+                .signWith(JwtConfig.key)
+                .compact();
+    }
+
+    public String getAccessTokenFromHeader(HttpServletRequest request) {
+
+        String bearerToken = request.getHeader(JwtConfig.ACCESS_TOKEN_HEADER);
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(JwtConfig.BEARER_PREFIX)) {
+            return bearerToken.replace(JwtConfig.BEARER_PREFIX, "");
+        }
+
+        return null;
+    }
+
+    public String getRefreshTokenFromHeader(HttpServletRequest request) {
+
+        String bearerToken = request.getHeader(JwtConfig.REFRESH_TOKEN_HEADER);
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(JwtConfig.BEARER_PREFIX)) {
+            return bearerToken.replace(JwtConfig.BEARER_PREFIX, "");
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+
+        try {
+            Jwts.parserBuilder().setSigningKey(JwtConfig.key).build().parseClaimsJws(token);
+            return true;
+        } catch (UnsupportedJwtException | MalformedJwtException | SignatureException e) {
+            return false;
+        } catch (ExpiredJwtException e) {
+            return false;
+        }
+    }
+
+    public Claims getUserInfoFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(JwtConfig.key).build().parseClaimsJws(token).getBody();
+    }
+}
