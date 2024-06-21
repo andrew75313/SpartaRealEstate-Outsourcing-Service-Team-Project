6033f0c809b23dcd4810bf85895e1041602bcd13
Hyeonjin Kim <andrew75313@gmail.com>
2024-06-21 20:46:07 +0900

    fix: EnableJpaAuditing 설정 추가
    -Timestamped를 사용하기 위한 설정

--- src/main/java/com/sparta/realestatefeed/RealEstateFeedApplication.java
+++ src/main/java/com/sparta/realestatefeed/RealEstateFeedApplication.java
@@ -2,8 +2,10 @@ package com.sparta.realestatefeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RealEstateFeedApplication {
 
     public static void main(String[] args) {
