2df87c81ed069e7df1f5cf2a1735f1ed15cf2efe
Rohtable <rohtable@gmail.com>
2024-06-21 14:46:36 +0900

    feat : 아파트 피드 조회

--- src/main/java/com/sparta/realestatefeed/service/ApartService.java
+++ src/main/java/com/sparta/realestatefeed/service/ApartService.java
@@ -8,6 +8,9 @@ import com.sparta.realestatefeed.repository.ApartRepository;
 import com.sparta.realestatefeed.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class ApartService {
@@ -22,5 +25,17 @@ public class ApartService {
         Apart apart = new Apart(requestDto, user); // 새 생성자를 사용하여 객체 생성
         Apart savedApart = apartRepository.save(apart);
         return new ApartResponseDto(savedApart);
+    }
+
+    public ApartResponseDto getApart(Long id) {
+        Apart apart = apartRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Invalid apart ID")
+        );
+        return new ApartResponseDto(apart);
+    }
+
+    public List<ApartResponseDto> getAllAparts() {
+        List<Apart> aparts = apartRepository.findAll();
+        return aparts.stream().map(ApartResponseDto::new).collect(Collectors.toList());
     }
 }
