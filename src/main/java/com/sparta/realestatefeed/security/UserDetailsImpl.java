249f01ff364a81463d8076a0f73af1506021dc5a
Hyeonjin Kim <andrew75313@gmail.com>
2024-06-24 10:32:15 +0900

    fix: 컨벤션에 맞춰서 코드 정리

--- src/main/java/com/sparta/realestatefeed/security/UserDetailsImpl.java
+++ src/main/java/com/sparta/realestatefeed/security/UserDetailsImpl.java
@@ -28,11 +28,13 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public String getUsername() {
+
         return user.getUserName();
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+
         UserRoleEnum role = user.getRole();
         String authority = role.getAuthority();
 
@@ -45,21 +47,25 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
+
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
+
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
+
         return true;
     }
 
     @Override
     public boolean isEnabled() {
+
         return true;
     }
 }
